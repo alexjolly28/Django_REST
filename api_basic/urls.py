cd36be6db94770acdce23730c6d7bb6cc3b75aee
alexjolly28 <alexjolly28@gmail.com>
2020-02-17 12:44:30 +0530

    completed

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import article_list, article_detail,article_list_api,api_details
+from .views import article_list, article_detail,article_list_api,api_details,ArticleApiView,ArticleDetails
 
 urlpatterns = [
     path('article/', article_list),
     path('api/',article_list_api),
     path('detail/<int:pk>/', article_detail),
-    path('api/<int:pk>/',api_details)
+    path('api/<int:pk>/',api_details),
+    path('article_api/',ArticleApiView.as_view()),
+    path('article_api/<int:id>/',ArticleDetails.as_view())
 ]
